f9e1da283da5dc0db7cfc50e0c858fd3f9859fba
Денис Котелев <deniskotelev@MBP-Denis.Dlink>
2023-09-27 22:09:47 +0300

    init

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import Link from 'next/link';
 import Container from '../Container';
+import styles from './styles.module.css';
+
+const menuLiks = [
+  {
+    title: 'Use cases',
+    href: '#usecase',
+  },
+  {
+    title: 'Features',
+    href: '#features',
+  },
+  {
+    title: 'How to start',
+    href: '#howtostart',
+  },
+];
 
 function Header() {
   return (
-    <header>
+    <header
+      id="header"
+      className={styles.header}
+    >
       <Container>
-        123123
+        <div className={styles.wrap}>
+          <div>
+            logo
+          </div>
+
+          <nav className={styles.menu}>
+            <ul>
+              {menuLiks.map((link) => (
+                <li key={link.href}>
+                  <Link href={link.href}>
+                    {link.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
       </Container>
     </header>
   );
