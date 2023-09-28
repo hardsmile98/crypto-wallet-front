f07822a0edbdd62da199b7398d612912e0ba0abf
Денис Котелев <deniskotelev@MBP-Denis.Dlink>
2023-09-28 22:54:36 +0300

    init

--- src/sections/Offer/index.tsx
+++ src/sections/Offer/index.tsx
@@ -3,7 +3,35 @@ import mobile from 'assets/images/mobile.webp';
 import Container from '@components/Container';
 import ButtonLink from '@components/ButtonLink';
 import Image from 'next/image';
+import badge1 from '@assets/images/badges/1.webp';
+import badge2 from '@assets/images/badges/2.webp';
+import badge3 from '@assets/images/badges/3.webp';
+import badge4 from '@assets/images/badges/4.webp';
+import badge5 from '@assets/images/badges/5.webp';
 import styles from './styles.module.css';
+
+const badges = [
+  {
+    id: 1,
+    image: badge1,
+  },
+  {
+    id: 2,
+    image: badge2,
+  },
+  {
+    id: 3,
+    image: badge3,
+  },
+  {
+    id: 4,
+    image: badge4,
+  },
+  {
+    id: 5,
+    image: badge5,
+  },
+];
 
 function Offer() {
   return (
@@ -30,6 +58,18 @@ function Offer() {
             </div>
 
             <div>
+              <div className={styles.badges}>
+                {badges.map((badge, idx) => (
+                  <Image
+                    className={idx !== 0 ? styles.badge : undefined}
+                    key={badge.id}
+                    priority
+                    src={badge.image}
+                    alt="bot"
+                  />
+                ))}
+              </div>
+
               <p>
                 Popular bots use Crypto Pay
               </p>
